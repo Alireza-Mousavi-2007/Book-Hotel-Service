5c71c1c5f6eb3016659f677d0d9dbad33395040c
alireza <alirezamousaviseyed1386@gmail.com>
2026-08-20 19:00:33 +0330

    properties added

---
+++ src/main/java/org/bookhotel/bookinghotelsystem/BookingHotelSystemApplication.java
@@ -0,0 +1,13 @@
+package org.bookhotel.bookinghotelsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingHotelSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingHotelSystemApplication.class, args);
+    }
+
+}
